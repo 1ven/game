67de4261b0387300e7409adf19c2079e3c3254f9
Ivan Mironchik <me.roaiven@gmail.com>
2016-08-27 17:00:33 +0300

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: "cheap-module-eval-source-map",
+  entry: [
+    "webpack-dev-server/client?http://localhost:3000",
+    "webpack/hot/only-dev-server",
+    "./src/index.ts",
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  module: {
+    loaders: [{
+      test: /\.tsx?$/,
+      loader: "ts-loader",
+    }],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+};
